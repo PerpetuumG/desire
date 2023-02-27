0ad54844bdf91f9cbfb47eebe262bb63ec14a7e2
PerpetuumG <perpetuumg@gmail.com>
2023-02-27 13:12:49 +0200

    create page 'blog'

--- app/js/main.js
+++ app/js/main.js
@@ -21,6 +21,13 @@ $(function () {
     arrows: false,
   });
 
+  $('.article-slider__box').slick({
+    prevArrow:
+      '<button type="button" class="article-slider__arrow article-slider__arrowleft"><image src="images/arrow-slide-left.svg" alt="arrow left"></button>',
+    nextArrow:
+      '<button type="button" class="article-slider__arrow article-slider__arrowright"><image src="images/arrow-slide-right.svg" alt="arrow right"></button>',
+  });
+
   var mixer = mixitup('.gallery__inner', {
     load: {
       filter: '.living',
